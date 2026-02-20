fb0eba9717faf5e2588cd036908795bb672936b3
gaurav13407 <gaurav9997961651jjk277584@outlook.com>
2026-02-20 14:57:17 +0530

    void_wm opens on screen with startup app

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,27 @@
 #include "renderer/renderer.hpp"
 #include "renderer/background.hpp"
 #include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     std::cout << "void_wm starting...\n\n";
+
+    // Parse arguments
+    // -s "command" launches an app when compositor starts
+    char *startup_cmd = nullptr;
+    int c;
+    while ((c = getopt(argc, argv, "s:h")) != -1) {
+        switch (c) {
+            case 's':
+                startup_cmd = optarg;
+                break;
+            case 'h':
+            default:
+                std::cout << "Usage: void_wm [-s startup_command]\n";
+                return 0;
+        }
+    }
 
     // Detect GPU
     RendererInfo info = Renderer::detect();
@@ -20,6 +38,14 @@ int main() {
     if (!server.init()) {
         std::cerr << "Failed to init server\n";
         return 1;
+    }
+
+    // Launch startup app if provided
+    if (startup_cmd) {
+        std::cout << "Launching: " << startup_cmd << "\n";
+        if (fork() == 0) {
+            execl("/bin/sh", "/bin/sh", "-c", startup_cmd, (void*)nullptr);
+        }
     }
 
     server.run();
